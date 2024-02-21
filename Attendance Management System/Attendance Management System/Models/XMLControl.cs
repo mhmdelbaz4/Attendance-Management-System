3ab7f739eb98bf84494c90f2cb5f800e53b570a7
Muhammad Ashraf <muhammad.ashraf.tahaa@gmail.com>
2024-02-21 02:49:12 +0200

    add crud functions on XML

--- Attendance Management System/Attendance Management System/Models/XMLControl.cs
+++ Attendance Management System/Attendance Management System/Models/XMLControl.cs
@@ -9,6 +9,14 @@ namespace Attendance_Management_System.Models
 {
     internal class XMLControl
     {
+        private static string XML_PATH  = Directory.GetParent("./../../../..")?.FullName + "/xml/attendance.xml";
+
+        public static string GetXMLPath()
+        {
+            return XML_PATH;
+        }
+
+        
         public static XmlDocument ReadAllDocument()
         {
             XmlDocument XmlDoc = new XmlDocument();
@@ -28,6 +36,12 @@ namespace Attendance_Management_System.Models
             return list;
         }
 
+        //get multiple nodes
+        public static XmlNodeList GetMultipleNodes(string nodePath)
+        {
+            XmlDocument XmlDoc = ReadAllDocument();
+            return XmlDoc.SelectNodes(nodePath);
+        }
         
 
         public static List<List<string>> MultibleNodesToList(string nodepath, string target, params string[] targets)
@@ -50,9 +64,50 @@ namespace Attendance_Management_System.Models
 
         public static XmlNode? GetNode(XmlDocument XmlDoc, string nodePath, string target, string value)
         {
-            string xPath = string.Format("{0}[{1}='{2}']", nodePath, target, value);
+            string xPath = $"{nodePath}[{target}='{value}']";
             XmlNode? userNode = XmlDoc.SelectSingleNode(xPath);
             return userNode;
+        }
+        //add node
+        public static void AddNode(string nodePath, string[] targets, string[] values)
+        {
+            XmlDocument XmlDoc = ReadAllDocument();
+            XmlNode root = XmlDoc.DocumentElement;
+            XmlNode? node = root.SelectSingleNode(nodePath);
+            if (node != null)
+            {
+                XmlNode newNode = XmlDoc.CreateElement(nodePath.Split('/')[nodePath.Split('/').Length - 1]);
+                for (int i = 0; i < targets.Length; i++)
+                {
+                    XmlNode target = XmlDoc.CreateElement(targets[i]);
+                    target.InnerText = values[i];
+                    newNode.AppendChild(target);
+                }
+                node.AppendChild(newNode);
+                XmlDoc.Save(Directory.GetParent("./../../../..")?.FullName + "/xml/attendance.xml");
+            }
+        }
+        //update node
+        public static void UpdateNode(string nodePath, string target, string value, string newValue)
+        {
+            XmlDocument XmlDoc = ReadAllDocument();
+            XmlNode? node = GetNode(XmlDoc, nodePath, target, value);
+            if (node != null)
+            {
+                node.SelectSingleNode(target).InnerText = newValue;
+                XmlDoc.Save(Directory.GetParent("./../../../..")?.FullName + "/xml/attendance.xml");
+            }
+        }
+        //delete node
+        public static void DeleteNode(string nodePath, string target, string value)
+        {
+            XmlDocument XmlDoc = ReadAllDocument();
+            XmlNode? node = GetNode(XmlDoc, nodePath, target, value);
+            if (node != null)
+            {
+                node.ParentNode?.RemoveChild(node);
+                XmlDoc.Save(Directory.GetParent("./../../../..")?.FullName + "/xml/attendance.xml");
+            }
         }
     }
 }
